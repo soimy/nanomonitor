430e49bca234d5fc9800099deeb6b20d789038fb
Shen Yiming <soimy@163.com>
2015-07-06 21:10:19 +0800

    Update the nettraffic.sh script to handle absolute path dynamically. Minor tweak on the webpage for better display. Reform the repo's structure

--- src/nettraffic.sh
+++ src/nettraffic.sh
@@ -4,7 +4,10 @@
 
 INTERVAL=5
 LOG_MAXLINE=20
-LOG_FILE="/home/sym/traffic.csv"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LOG_FILE=$DIR/traffic.csv
+
+echo "Output traffic CSV file to : " $LOG_FILE
 
 if [[ "$#" -eq 0 ]]; then
 	echo "Usage: nettraffic [[Interface Name]..]"
@@ -12,17 +15,13 @@ if [[ "$#" -eq 0 ]]; then
 	exit
 fi
 
-#define timestamp function
-#timestamp() {
-	#date +"%T"
-#}
 TIMESTAMP=$(date +%H:%M)
 
 #tail the log file to LOG_MAXLINE
 if [ ! -f $LOG_FILE ]; then
 	touch $LOG_FILE
 else
-	tail -$LOG_MAXLINE $LOG_FILE > /home/sym/tmp.log && mv /home/sym/tmp.log $LOG_FILE
+	tail -$LOG_MAXLINE $LOG_FILE > $DIR/tmp.log && mv $DIR/tmp.log $LOG_FILE
 fi
 
 for i in "$@"
